8b17e6eec82c925a402bb549256b9346eb2a84dc
Rohan K <84638053+vaibruce@users.noreply.github.com>
2025-01-04 12:53:59 +0530

    Update 7 - adder2-design a 1-bit adder.v

--- 3 - Modules/7 - adder2-design a 1-bit adder.v
+++ 3 - Modules/7 - adder2-design a 1-bit adder.v
@@ -1,14 +1,18 @@
-module top_module (
+module top_module(
     input [31:0] a,
     input [31:0] b,
     output [31:0] sum
-);//
-	wire con1, con2;
-    add16 adder_1(a[15:0], b[15:0], 0, sum[15:0], con1);
-    add16 adder_2(a[31:16], b[31:16], con1, sum[31:16], con2);
+);
+    reg cin;
+    add16 a1 (a[15:0],b[15:0],1'b0,sum[15:0],cin);
+    add16 a2 (a[31:16],b[31:16],cin, sum[31:16]);
+
 endmodule
 
+
 module add1 ( input a, input b, input cin,   output sum, output cout );
-    assign sum  = a^b^cin;
-    assign cout = (a&b)|(a&cin)|(b&cin);
+    
+assign sum = a ^ b ^ cin;
+assign cout = a&b | a&cin | b&cin;
+
 endmodule
