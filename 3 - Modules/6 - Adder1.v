3e08674d673f63cefbe4fdf12d5f7d1c728f8f50
Rohan K <84638053+vaibruce@users.noreply.github.com>
2025-01-04 12:48:10 +0530

    Update 6 - Adder1.v

--- 3 - Modules/6 - Adder1.v
+++ 3 - Modules/6 - Adder1.v
@@ -3,8 +3,8 @@ module top_module(
     input [31:0] b,
     output [31:0] sum
 );
-    wire con1, con2;
-    add16 adder_1(a[15:0], b[15:0], 0, sum[15:0], con1);
-    add16 adder_2(a[31:16], b[31:16], con1, sum[31:16], con2);
+    reg cin;
+    add16 a1 (a[15:0],b[15:0],1'b0,sum[15:0],cin);
+    add16 a2 (a[31:16],b[31:16],cin, sum[31:16]);
 
 endmodule
