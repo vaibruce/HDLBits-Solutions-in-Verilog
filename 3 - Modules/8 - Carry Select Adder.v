4aa35da3193972a18a34fd5f54e8e873f6f006ce
Rohan K <84638053+vaibruce@users.noreply.github.com>
2025-01-04 13:06:49 +0530

    Update 8 - Carry Select Adder.v

--- 3 - Modules/8 - Carry Select Adder.v
+++ 3 - Modules/8 - Carry Select Adder.v
@@ -1,19 +1,16 @@
 module top_module(
     input [31:0] a,
     input [31:0] b,
-    output reg [31:0] sum
+    output [31:0] sum
 );
-    wire [15:0] cout,con2;
-    wire [15:0]alt_sum1, alt_sum2;
-    add16 adder1(a[15:0], b[15:0], 0, sum[15:0], cout);
-    add16 adder_sel1(a[31:16], b[31:16], 0, alt_sum1, con2);
-    add16 adder_sel2(a[31:16], b[31:16], 1, alt_sum2, con2);
-    
-    always @(cout, alt_sum1, alt_sum2) begin
-        case(cout)
-            0 : sum[31:16] = alt_sum1;
-            1 : sum[31:16] = alt_sum2;
-        endcase
-    end
 
+    reg cin;
+    wire [15:0] sum1,sum0;
+    add16 a1 (a[15:0],b[15:0],1'b0,sum[15:0],cin);
+    add16 a2 (a[31:16],b[31:16],1'b0, sum0[15:0]);
+    add16 a3 (a[31:16],b[31:16],1'b1, sum1[15:0]);
+    
+    assign sum [31:16]=cin?sum1:sum0;
+    
+    
 endmodule
