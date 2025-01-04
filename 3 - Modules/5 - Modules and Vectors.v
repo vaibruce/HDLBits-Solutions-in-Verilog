14b0977ce8a3b2ba942c3cdb26052bbe028366ff
Rohan K <84638053+vaibruce@users.noreply.github.com>
2025-01-04 12:45:55 +0530

    Update 5 - Modules and Vectors.v

--- 3 - Modules/5 - Modules and Vectors.v
+++ 3 - Modules/5 - Modules and Vectors.v
@@ -2,31 +2,20 @@ module top_module (
     input clk, 
     input [7:0] d, 
     input [1:0] sel, 
-    output reg [7:0] q 
+    output [7:0] q 
 );
-    wire [7:0]con1,con2,con3;
-    
-    my_dff8 d_flop1(.clk(clk), .d(d), .q(con1));
-    my_dff8 d_flop2(.clk(clk), .d(con1), .q(con2) );
-    my_dff8 d_flop3(.clk(clk), .d(con2), .q(con3));
-    
-    always @ (*) begin
-        case(sel)
-            0 : q = d ;
-            1 : q = con1;
-            2 : q = con2;
-            3 : q = con3;
-        endcase
-        
-        /*if(sel==2'b00)
-            q=d;
-        else if( sel == 2'b01)
-      		q = con1;
-        else if( sel == 2'b10)
-      		q = con2;
-        else if( sel == 2'b11)
-      		q = con3;
- 		*/
+    wire [7:0] d1,d2,d3;
+    my_dff8 d1(clk,d,d1);
+    my_dff8 d2(clk,d1,d2);
+    my_dff8 d3(clk,d2,d3);
+    always@(*)
+        begin
+            case(sel)
+                2'b00:q=d;
+                2'b01:q=d1;
+                2'b10:q=d2;
+                2'b11:q=d3;
+            endcase
  	end
-    
+    //assign out = sel[1] ? (sel[0] ? d3 : d2) : (sel[0] ? d1 : d);
 endmodule
